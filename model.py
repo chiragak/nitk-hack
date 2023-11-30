ca8998808caf83fff2d83ec2d604eba09eb43acf
Chirag A K <chiragajekar@gmail.com>
2023-12-01 01:20:21 +0530

    update

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
-hi testing 
+import os
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+import matplotlib.pyplot as plt
+
